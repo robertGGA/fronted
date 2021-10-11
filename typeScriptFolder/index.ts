e3bae6e0be4e985046ca61efcc5569ae78e1a325
robert <78978033+robertGGA@users.noreply.github.com>
2021-10-12 01:25:39 +0300

    bugFixed

---
+++ typeScriptFolder/index.ts
@@ -0,0 +1,18 @@
+interface Todo {
+    title: string;
+    description: string;
+}
+
+function updateTodo(todo: Todo, fieldsToUpdate: Partial<Todo>) {
+    console.log({ ...todo, ...fieldsToUpdate });
+    return { ...todo, ...fieldsToUpdate };
+}
+
+const todo1 = {
+    title: "organize desk",
+    description: "clear clutter",
+};
+
+const todo2 = updateTodo(todo1, {
+    description: "throw out trash",
+});
